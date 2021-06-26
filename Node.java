2e99fb1439651eeb09355e025c0b4cd503c07e6c
ashish29cs <ashishb.mca19@rvce.edu.in>
2021-06-26 20:27:38 +0530

    updated

--- Node.java
+++ Node.java
@@ -3,6 +3,7 @@ package BinaryTree;
 public class Node {
 	int data;
 	Node left,right;
+	int height;
 	public Node(int data){
 		this.data=data;
 		left=null;
--- SumAtKthLevel.java
+++ SumAtKthLevel.java
@@ -4,6 +4,8 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
+import java.util.TreeMap;
 
 
 
@@ -151,9 +153,272 @@ public class SumAtKthLevel {
 		 
 		 return Math.max(lh,rh)+1;
 	 }
+	public int getDifferenceEvenOddValues(Node root)
+	{
+		if(root==null)
+		{
+			return 0;
+		}
+		return root.data-getDifferenceEvenOddValues(root.left)-getDifferenceEvenOddValues(root.right);
+	}
+	public int getNumberofLeafNode(Node root)
+	{
+		if(root==null)
+		{
+			return 0;
+		}
+		if(root.left==null && root.right==null)
+		{
+			return 1;
+		}
+		return getNumberofLeafNode(root.left)+getNumberofLeafNode(root.right);
+	}
+	public void printValueAtGivenlevel(Node root,int level)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		if(level==1)
+		{
+			System.out.print(root.data + " ");
+			return;
+		}
+		
+		printValueAtGivenlevel(root.left,level-1);
+		printValueAtGivenlevel(root.right,level-1);
+	}
 	
 	
+	public void reverseLevelOrder(Node root)
+	{
+		Node node;
+		if(root==null)
+		{
+			return;
+		}
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
+		Stack<Node> s = new Stack<>();
+		while(q.size()>0)
+		{
+		node=q.peek();
+		q.remove();
+		
+			if(node.right!=null){
+				q.add(node.right);
+				
+			}
+			if(node.left!=null)
+			{
+				q.add(node.left);
+			}
+			s.push(node);
+		}
+		while(s.size()>0)
+		{
+			node=s.peek();
+			System.out.print(node.data +" ");
+			s.pop();
+			
+		}
+		
+	}
 	
+	
+	void sumReplacement(Node root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		sumReplacement(root.left);
+		sumReplacement(root.right);
+		
+		
+		if(root.left!=null)
+		{
+			root.data+=root.left.data;
+		}
+
+		if(root.right!=null)
+		{
+			root.data+=root.right.data;
+		}
+
+	}
+	
+	void preorder(Node root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		System.out.print(root.data+" ");
+		preorder(root.left);
+		preorder(root.right);
+	}
+	
+	
+	public void printBoundary(Node node) {
+	    if (node != null) {
+	      System.out.print(node.data + " ");
+
+	      printBoundaryLeft(node.left);
+
+	      printLeaves(node.left);
+	      printLeaves(node.right);
+
+	      printBoundaryRight(node.right);
+	    }
+	  }
+	  
+	  public void printBoundaryLeft(Node node) {
+	    if (node == null) {
+	      return;
+	    }
+
+	    if (node.left != null) {
+	      System.out.print(node.data + " ");
+	      printBoundaryLeft(node.left);
+	    } else if (node.right != null) {
+	      System.out.print(node.data + " ");
+	      printBoundaryLeft(node.right);
+	    }
+	  }
+
+	  public void printLeaves(Node node) {
+	    if (node == null) {
+	      return;
+	    }
+
+	    printLeaves(node.left);
+
+	    if (node.left == null && node.right == null) {
+	      System.out.print(node.data + " ");
+	    }
+	    printLeaves(node.right);
+	  }
+
+	  public void printBoundaryRight(Node node) {
+	    if (node == null) {
+	      return;
+	    }
+
+	    if (node.right != null) {
+	      printBoundaryRight(node.right);
+	      System.out.print(node.data + " ");
+	    } else if (node.left != null) {
+	      printBoundaryRight(node.left);
+	      System.out.print(node.data + " ");
+	    }
+	  }
+	  
+	  
+	  public  void printzigzagTraversal(Node root)
+	  {
+		  Stack<Node> s1=new Stack<Node>();
+		  Stack<Node> s2=new Stack<Node>();
+		  
+		  s1.push(root);
+		  while(!s1.isEmpty() || !s2.isEmpty())
+		  {
+			  while(!s1.isEmpty())
+			  {
+				  Node tmp=s1.pop();
+				  System.out.print(tmp.data +" ");
+				  
+				  if(tmp.left!=null)
+				  {
+					  s2.push(tmp.left);
+				  }
+				  if(tmp.right!=null)
+				  {
+					  s2.push(tmp.right);
+				  }
+			  }
+			  System.out.println();
+			  while(!s2.isEmpty())
+			  {
+				  Node tmp=s2.pop();
+				  System.out.print(tmp.data +" ");
+				  
+				  if(tmp.right!=null)
+				  {
+					  s1.push(tmp.right);
+				  }
+				  if(tmp.left!=null)
+				  {
+					  s1.push(tmp.left);
+				  }
+			  }
+			  System.out.println();
+			  
+		  }
+		  
+	  }
+	  
+	  
+	  public void topView(Node node) {
+		    if (node == null) {
+		      return;
+		    }
+
+		    TreeMap<Integer, Integer> m = new TreeMap<Integer, Integer>();
+
+		    Queue<Node> q = new LinkedList<Node>();
+		    q.add(node);
+
+		    while (!q.isEmpty()) {
+		      Node temp = q.remove();
+		      int hd = temp.height;
+		      
+		      if (m.get(hd) == null) {
+		        m.put(hd, temp.data);
+		      }
+
+		      if (temp.left != null) {
+		        temp.left.height = hd - 1;
+		        q.add(temp.left);
+		      }
+
+		      if (temp.right != null) {
+		        temp.right.height = hd + 1;
+		        q.add(temp.right);
+		      }
+		    }
+		    System.out.println(m.values());
+		  }
+
+	  
+	  public void bottomView(Node node) {
+		    if (node == null) {
+		      return;
+		    }
+
+		    TreeMap<Integer, Integer> m = new TreeMap<Integer, Integer>();
+
+		    Queue<Node> q = new LinkedList<Node>();
+		    q.add(node);
+
+		    while (!q.isEmpty()) {
+		      Node temp = q.remove();
+		      int hd = temp.height;
+		      
+		      m.put(hd, temp.data);
+		  
+		      if (temp.left != null) {
+		        temp.left.height = hd - 1;
+		        q.add(temp.left);
+		      }
+
+		      if (temp.right != null) {
+		        temp.right.height = hd + 1;
+		        q.add(temp.right);
+		      }
+		    }
+		    System.out.println(m.values());
+		  }
 	
 
 	public static void main(String[] args) {
@@ -182,6 +447,10 @@ public class SumAtKthLevel {
 		ob.levelOrder(root);
 		System.out.println(arr);
 		
+		ob.topView(root);
+		
+		ob.bottomView(root);
+		
 		
 		System.out.println(ob.sumOfAllNodes(root));
 		System.out.println(ob.count(root));
@@ -198,12 +467,33 @@ public class SumAtKthLevel {
 		ob.optimiseDiameter(root);
 		System.out.println(ob.diameter);
 		
+		System.out.println(ob.getDifferenceEvenOddValues(root));
+		
+		
+		System.out.println(ob.getNumberofLeafNode(root));
+		
+		ob.printValueAtGivenlevel(root,3);
+		
+		
+		ob.reverseLevelOrder(root);
+		
+		System.out.println();
+		
+		ob.preorder(root);
+		System.out.println();
+		ob.sumReplacement(root);
+		ob.preorder(root);
+		System.out.println();
+		
+		
+		ob.printBoundary(root);
+		System.out.println();
 		
 		
 		
+		ob.printzigzagTraversal(root);
 		
-		
-		
+		System.out.println();
 		
 		
 
